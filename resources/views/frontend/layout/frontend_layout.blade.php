dd4474556a800c35165906109e0b5decdba19e7f
Jayaram Dhungana <jayaramdhungana104@gmail.com>
2026-03-02 22:49:26 +0545

    try to solve css issue

--- resources/views/frontend/layout/frontend_layout.blade.php
+++ resources/views/frontend/layout/frontend_layout.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')-DJ</title>
   <link rel="icon" href="{{ asset('images/logo/dj_logo_fit-removebg-preview.png') }}" type="image/png">
-  <link rel="stylesheet" href="{{ asset('css/frontend/layout.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/frontend/layout.css') }}?v={{ time() }}">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
   @yield('styles')
 
